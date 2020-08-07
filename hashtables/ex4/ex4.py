cdfb98e03ccf234ac956dc7e6718fdde3a28c8de
Rachele Edwards <dot.com1472@gmail.com>
2020-08-07 09:06:18 -0600

    commented pseudo code for negatives, coded and pass on first try--AHHHH like what

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,25 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    #variables needed: result = list to return, neg = list for negative numbers
+    result = []
+    neg = []
+    #check list of arrays
+    for item in a:
+        #if number is less than 0, its a neg
+        if item < 0:
+            #add to neg list
+            neg.append(item)
+    #check number in neg list
+    for num1 in neg:
+        #check number in array list
+        for num2 in a:
+            #if array number == the absolute number of the negative number
+            if num2 == abs(num1):
+                #add to result
+                result.append(num2)
 
+    #return result
     return result
 
 
