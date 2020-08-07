685dc0fd9a96408cc884c2c8b3c4021c98bf1c23
Rachele Edwards <dot.com1472@gmail.com>
2020-08-07 09:12:33 -0600

    adding pseudocode to intersection before implementing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,7 +3,19 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
-
+    #variables needed: result = list for end result, and cache = dict to check numbers
+    result = []
+    cache = {}
+    #check each array in arrays
+        #check each number in array
+            #if the number is in cache
+                #add 1 to cache
+            #otherwise, cache[num] = 0
+    #have a cache with key, values
+    #wanting to find the key with the most values
+        #if value is the len of arrays,
+            #add the key to result list
+    #return result
     return result
 
 
