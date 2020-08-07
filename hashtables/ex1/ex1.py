2b719439ca245c0e116fb1e72d2b6fd9b415096a
Rachele Edwards <dot.com1472@gmail.com>
2020-08-07 10:44:34 -0600

    trying to do naive solution to figure out better solution

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,20 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    #variables needed: ht = {}, result = tuple, index for index in list
+    for weight1 in weights:
+        for weight2 in weights:
+            if limit - weight2 == weight1:
+                if weight1 < weight2:
+                    result = (weight2, weight1)
+                else:
+                    result = (weight1, weight2)
+            else:
+                
 
-    return None
+    return result
+
+
+weights = [ 4, 6, 10, 15, 16 ]
+
+get_indices_of_item_weights(weights, 5, 21)
